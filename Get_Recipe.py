effd861445e112e8063e24440ec84bb7d5635790
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-05-31 19:31:44 +0200

    new input: user mood

--- Get_Recipe.py
+++ Get_Recipe.py
@@ -4,7 +4,7 @@ import openai
 from front import make_space
 
 st.title("From ingredients to Recipes")
-st.markdown("""##### This app generates recipes and pictures of the meals based on the ingredients you have at home""")
+st.markdown("""##### This app generates recipes and pictures of the meals based on the ingredients you have at home. You can use the "Find a recipe" button several times to get different ideas.""")
 make_space(3)
 
 #define key
@@ -16,9 +16,10 @@ system_msg = """You're a funny AI assistant who helps people find original recip
                 (meat-free, vegan, gluten-free, lactose-free etc, if specified). In addition, all your answers must contain humor and jokes."""
 system_msg_summary = """Describe in one sentence the meal created by describing precisely what it should look like"""
 
-#get user input
+#get user inputs
+meal_type = st.selectbox("What is your mood?", ["Pig out mood", "I want to eat clean", "Surprise me"])
 prompt = st.text_area("Enter your ingredients and (optionally) some preferences")
-prompt = "User ingredients: " + prompt + "\n\nAI assistant: "
+prompt = "User mood is: " + meal_type + "\n\n" + "User ingredients: " + prompt + "\n\nAI assistant: "
 
 #call gpt api and display output
 output_gpt = None
@@ -32,11 +33,12 @@ if run and len(prompt) > 5:
     #call dalle api and display output
     input_dalle = api_gpt(output_gpt, system_msg_summary)
     input_dalle = "4k detailed and high quality photo of " + input_dalle[17:].lower()
-    #st.write("Input Dall-e: ", input_dalle)
     make_space(1)
     st.markdown("### Generated picture of the recipe")
-
-    #call dalle api and display output
     api_dalle(input_dalle)
+    show_prompt = st.checkbox("Show prompt", value=True)
+    if show_prompt:
+        st.write("Input Dall-e: ", input_dalle)
+
 
 
